1aa0bb8da8c423f89ac5fcf56616d20af18cb056
Doantaihuynh100983 <doantai100983hu@gmail.com>
2022-03-03 16:53:09 +0700

    up bai

--- caseStudy-angular/src/app/app-routing.module.ts
+++ caseStudy-angular/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ListCustomerComponent} from './customer/list-customer/list-customer.component';
+import {AddCustomerComponent} from './customer/add-customer/add-customer.component';
+import {UpdateCustomerComponent} from './customer/update-customer/update-customer.component';
+import {ListEmployeeComponent} from './employee/list-employee/list-employee.component';
+import {AddEmployeeComponent} from './employee/add-employee/add-employee.component';
+import {UpdateEmployeeComponent} from './employee/update-employee/update-employee.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home', component: HomeComponent},
+  {path : '', component: HomeComponent},
+  {path : 'customerList', component: ListCustomerComponent},
+  {path : 'addCustomer' , component: AddCustomerComponent},
+  {path : 'updateCustomer' , component: UpdateCustomerComponent},
+  {path : 'listEmployee' , component: ListEmployeeComponent},
+  {path : 'addEmployee' , component: AddEmployeeComponent},
+  {path : 'updateEmployee' , component: UpdateEmployeeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- caseStudy-angular/src/app/app.module.ts
+++ caseStudy-angular/src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { HomeComponent } from './home/home.component';
+import { AddCustomerComponent } from './customer/add-customer/add-customer.component';
+import { ListCustomerComponent } from './customer/list-customer/list-customer.component';
+import { UpdateCustomerComponent } from './customer/update-customer/update-customer.component';
+import { ListEmployeeComponent } from './employee/list-employee/list-employee.component';
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    HomeComponent,
+    AddCustomerComponent,
+    ListCustomerComponent,
+    UpdateCustomerComponent,
+    ListEmployeeComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
